1a214888e281879e3468e2a6e3f36543b59d40a1
Surendhar <Surendhar@theproindia.com>
2024-05-10 11:47:08 +0530

    feat: match-toy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,28 +12,26 @@ export class AppComponent {
   title = 'Match-Toy';
   response = {
     status: 200,
-    user: {
-      name: 'John Doe',
-      age: 30,
-      address: {
-        city: 'New York',
-        country: 'USA',
-      },
-    },
+    // user: {
+    //   name: 'John Doe',
+    //   age: 30,
+    //   address: {
+    //     city: 'New York',
+    //     country: 'USA',
+    //   },
+    // },
   };
   message: string = '';
 
   // checkResponse(response: any) {
   //   console.log(response);
+
   getUserResponse = match
-    .case(
-      '{ status: 200, user: { name: string, ...address } }',
-      ({ data }: { data: any }) =>
-        (this.message = `User name is ${data?.status} and lives on ${data?.user?.address?.city}`)
-    )
+    .case(`{ status: Number }`, () => (this.message = `one  `))
     .else(() => 'No user found')
     .catch((e: any) => console.log('Error on getUserResponse', e))
     .end();
+
   // }
   // try {
   //   if (this.response && this.response.status === 200) {
