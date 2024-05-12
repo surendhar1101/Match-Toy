caf26736e3cde1bf9d34852e26aec7e3ce6a88ba
Surendhar <Surendhar@theproindia.com>
2024-05-12 22:49:33 +0530

    match-toy succesfully implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { match } from 'match-toy';
 
-// const match = require('match-toy');
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,51 +10,51 @@ export class AppComponent {
   title = 'Match-Toy';
   response = {
     status: 200,
-    // user: {
-    //   name: 'John Doe',
-    //   age: 30,
-    //   address: {
-    //     city: 'New York',
-    //     country: 'USA',
-    //   },
-    // },
+    user: {
+      name: 'Surendhar',
+      age: 30,
+      address: {
+        city: 'New York',
+        country: 'USA',
+      },
+    },
   };
   message: string = '';
 
-  // checkResponse(response: any) {
-  //   console.log(response);
-
   getUserResponse = match
-    .case(`{ status: Number }`, () => (this.message = `one  `))
-    .else(() => 'No user found')
+    .case(
+      `{ status, user: { name, age, address: { city, country } }}`,
+      ({ name, age, city }: any) => {
+        this.message = `User name is ${name} at ${age} from ${city}`;
+      }
+    )
+    .else(() => (this.message = 'No user found'))
     .catch((e: any) => console.log('Error on getUserResponse', e))
     .end();
 
-  // }
-  // try {
-  //   if (this.response && this.response.status === 200) {
-  //     if (this.response.user) {
-  //       let name;
-  //       let address;
-  //       if (
-  //         this.response.user.name &&
-  //         typeof this.response.user.name === 'string'
-  //       ) {
-  //         name = this.response.user.name;
-  //       }
-  //       if (this.response.user.address) {
-  //         address = this.response.user.address.city;
-  //       }
-  //       if (name && address) {
-  //         this.message = `User name is ${name} and lives on ${address}`;
+  // getUserResponse(response: any) {
+  //   try {
+  //     if (response && response.status === 200) {
+  //       if (response.user) {
+  //         let name;
+  //         let address;
+  //         if (response.user.name && typeof response.user.name === 'string') {
+  //           name = response.user.name;
+  //         }
+  //         if (response.user.address) {
+  //           address = response.user.address.city;
+  //         }
+  //         if (name && address) {
+  //           this.message = `User name is ${name} and lives on ${address}`;
+  //         }
+  //       } else {
+  //         this.message = 'No user found';
   //       }
   //     } else {
   //       this.message = 'No user found';
   //     }
-  //   } else {
-  //     this.message = 'No user found';
+  //   } catch (e) {
+  //     console.log('Error on getUserResponse', e);
   //   }
-  // } catch (e) {
-  //   console.log('Error on getUserResponse', e);
   // }
 }
